7bef8cf6e4332514a666e365f3687a3828c7a373
n1zhon1 <kristineetsitty@gmail.com>
2021-08-29 12:16:20 -0700

    added first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
-var startQuiz = function(event) {
-    
+var startBtn = document.getElementById('startQuiz');
+var submitButton = document.getElementById('submit');
+
+document.getElementById("startBtn").addEventListener("click", startQuiz);
+
+function startQuiz() {
+
 }
+
+function endResults() {
+
+}
+
+//submit for results
+submitButton.addEventListener('click', showResults);
+
+//quiz questions
+var testQuestions = [
+    {
+        question: "Question 1",
+        answers: {
+            a: "answer 1",
+            b: "answer 2",
+            c: "answer 3",
+        },
+        correctAnswer: "a"
+    }
+];
